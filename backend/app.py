81e7ffc1e3bffe01b9b29187be39c6c6035fbd50
mrmateussiilva <mateuspython3010@gmail.com>
2025-03-22 21:13:41 -0300

    Api incompleta

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS
+CORS(app)  
 
 @app.route('/orders', methods=['GET'])
 def get_orders():
